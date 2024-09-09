f8bc08eb99034e9988fcc76f5ac8f6f7ab667091
bruyko-e <bruykoev@gmail.com>
2024-09-08 22:14:00 -0600

    Add my name in ViewController

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by brubru on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	let name = "Djony"
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		print("Меня зовут \(name), я создал свое первое приложение.")
+	}
+}
+
